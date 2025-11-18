5af7093dbb4dc3ce3e7c74ec47baaee75d66dfc5
Zaid Tariq <zaidtariqshah>
2025-11-18 14:05:01 +0530

    second commit

--- drizzle/src/index.ts
+++ drizzle/src/index.ts
@@ -1,2 +1,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
-export const db = drizzle(process.env.DATABASE_URL!);
+import { neon } from "@neondatabase/serverless";
+
+const client = neon(process.env.DATABASE_URL!);
+export const db = drizzle(client);
